7b645e15778f86e97e998db3848417236149e5eb
Hyrax01 <piora0702@gmail.com>
2025-11-20 20:25:04 +0900

    Create ReadingCalendarRepository.java

---
+++ starbooks/src/main/java/com/starbooks/domain/goal/ReadingCalendarRepository.java
@@ -0,0 +1,6 @@
+package com.starbooks.domain.goal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadingCalendarRepository extends JpaRepository<ReadingCalendar, Long> {
+}
