d0135106ae340c79dc4abb0cf37e6beaf258d323
Hyrax01 <piora0702@gmail.com>
2025-11-20 20:23:31 +0900

    Create GoalType.java

---
+++ starbooks/src/main/java/com/starbooks/domain/goal/GoalType.java
@@ -0,0 +1,8 @@
+package com.starbooks.domain.goal;
+
+public enum GoalType {
+    MONTHLY_BOOK_COUNT,   // 한 달 독서권수 목표
+    YEARLY_BOOK_COUNT,    // 연간 독서 목표
+    DAILY_READING_TIME,   // 하루 독서 시간 목표
+    WEEKLY_READING_TIME   // 주간 독서 시간 목표
+}
