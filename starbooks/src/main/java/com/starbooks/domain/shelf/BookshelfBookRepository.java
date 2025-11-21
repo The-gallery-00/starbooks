46de035c1c1ed930d6622a2ae8ff45b8b1d7d65d
Choi BeomSu <cvgy1915@naver.com>
2025-11-22 02:33:34 +0900

    Delete starbooks/src/main/java/com/starbooks/domain/shelf directory

--- starbooks/src/main/java/com/starbooks/domain/shelf/BookshelfBookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.starbooks.domain.shelf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookshelfBookRepository extends JpaRepository<BookshelfBook, BookshelfBookId> {
-
-}
