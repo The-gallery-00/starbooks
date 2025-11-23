439c66f880bd57921557f240fdbd9309727015a4
SeEun <oseeun60@gmail.com>
2025-11-24 00:09:35 +0900

    reading, community, challenge 수정

--- starbooks/src/main/java/com/starbooks/domain/challenge/ChallengeRepository.java
+++ starbooks/src/main/java/com/starbooks/domain/challenge/ChallengeRepository.java
@@ -2,5 +2,12 @@ package com.starbooks.domain.challenge;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+    // 상태별 검색 (ACTIVE, SCHEDULED 등)
+    List<Challenge> findByStatus(ChallengeStatus status);
+
 }
+
