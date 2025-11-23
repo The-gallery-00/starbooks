9769052753fa68fb6643c5d4590f2eecb52150e7
Choi BeomSu <cvgy1915@naver.com>
2025-11-23 21:44:10 +0900

    Add files via upload
    User, Book, Shelf, Auth dto upload

---
+++ starbooks/src/main/java/com/starbooks/DTO/user/UserRequest.java
@@ -0,0 +1,13 @@
+package com.starbooks.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private String nickname;
+}
