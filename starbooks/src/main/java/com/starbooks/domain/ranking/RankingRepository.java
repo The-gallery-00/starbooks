b789c81f9693cb3c2e122bcfe868fcd8cdfa5f11
SeEun <oseeun60@gmail.com>
2025-11-25 22:23:37 +0900

    challenge, goal, ranking, reading repository 추가

---
+++ starbooks/src/main/java/com/starbooks/domain/ranking/RankingRepository.java
@@ -0,0 +1,6 @@
+package com.starbooks.domain.ranking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
+}
