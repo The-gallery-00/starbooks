0bad3015dbe061d58cba864fc1d6fc775c30449a
Hyrax01 <piora0702@gmail.com>
2025-11-25 22:32:02 +0900

    user~bookshelf service

---
+++ starbooks/src/main/java/com/starbooks/service/user/UserServiceImpl.java
@@ -0,0 +1,45 @@
+// com.starbooks.service.user.UserServiceImpl.java
+package com.starbooks.service.user;
+
+import com.starbooks.domain.user.User;
+import com.starbooks.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User update(Long id, User updated) {
+        User user = findById(id);
+        user.setNickname(updated.getNickname());
+        user.setIntro(updated.getIntro());
+        user.setProfileImage(updated.getProfileImage());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
