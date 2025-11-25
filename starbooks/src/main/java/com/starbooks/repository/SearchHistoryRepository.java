71abd85900dfea697a050697878bbfc8d66ad9b5
Choi BeomSu <cvgy1915@naver.com>
2025-11-25 16:48:00 +0900

    Delete starbooks/src/main/java/com/starbooks/repository directory

--- starbooks/src/main/java/com/starbooks/repository/SearchHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.starbooks.repository;
-
-import com.starbooks.entity.SearchHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
-}
