b8af42eddb8f39a816015a7c66893a4c7e0c9574
The-gallery-00 <cvgy1915@naver.com>
2025-11-25 22:24:20 +0900

    Repository 추가

---
+++ starbooks/src/main/java/com/starbooks/domain/friend/FriendshipRepository.java
@@ -0,0 +1,7 @@
+// com.starbooks.domain.friend.FriendshipRepository
+package com.starbooks.domain.friend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+}
