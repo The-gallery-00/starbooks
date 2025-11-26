ce14a7900b74b396f1c5431e9c611ea44717c440
The-gallery-00 <cvgy1915@naver.com>
2025-11-26 15:22:50 +0900

    controller 추가

---
+++ starbooks/src/main/java/com/starbooks/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.starbooks.exception;
+
+public class BadRequestException extends StarbooksException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
