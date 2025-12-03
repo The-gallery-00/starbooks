8efa78f70edcc478286eb093e6a85f695591f011
Hyrax01 <piora0702@gmail.com>
2025-12-03 17:11:08 +0900

    알림기능 다시 손 보기 및 event class 추가

--- starbooks/src/main/java/com/starbooks/domain/user/User.java
+++ starbooks/src/main/java/com/starbooks/domain/user/User.java
@@ -55,6 +55,10 @@ public class User {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    public Long getId() {
+        return 0L;
+    }
+
 
     public enum Role {
         USER,
