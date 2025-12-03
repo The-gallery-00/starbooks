6057f1b7d77f6f2997dfabfb85a9827a67069524
The-gallery-00 <cvgy1915@naver.com>
2025-12-03 16:48:45 +0900

    config

--- starbooks/src/main/java/com/starbooks/dto/auth/JwtResponse.java
+++ starbooks/src/main/java/com/starbooks/dto/auth/JwtResponse.java
@@ -1,3 +1,4 @@
+// com.starbooks.dto.auth.JwtResponse
 package com.starbooks.dto.auth;
 
 import lombok.AllArgsConstructor;
@@ -6,7 +7,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class JwtResponse {
+
     private String token;
-    private String tokenType = "Bearer";
+    private String tokenType;
     private Long expiresIn; // ms
+    private UserInfo user;  // ⭐ 프론트가 원하는 user 정보
+
+    @Getter
+    @AllArgsConstructor
+    public static class UserInfo {
+        private Long id;
+        private String username;
+        private String email;
+        private String nickname;
+    }
 }
