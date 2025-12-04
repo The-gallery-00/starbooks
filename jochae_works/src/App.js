4210aef0811954d489eb6375222db703d32d259f
jochae <163301367+jochae@users.noreply.github.com>
2025-12-04 20:30:06 +0900

    Update App.js

--- jochae_works/src/App.js
+++ jochae_works/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { UserProvider } from "./UserContext";
+import ProtectedRoute from "./ProtectedRoute";
 
 import { Header } from "./Header";
 import PageLocation from "./PageLocation";
 import GlobalSearch from "./GlobalSearch";
 import Login from "./Login";
-import Home from "./Home";
+import Intro from "./Intro";
+import Home from "./Home"
 import MyLibrary from "./MyLibrary";
 import MyBookDetail from "./MyBookDetail";
 import BookInfo from "./BookInfo";
@@ -20,32 +22,131 @@ import FriendLibrary from "./FriendLibrary";
 import Notification from "./Notification";
 import TopButton from "./TopButton";
 
-
 function AppContent() {
   const location = useLocation();
 
   const showPageLocation = !["/"].includes(location.pathname);
 
+  const showHeader = location.pathname !== "/";
+
   return (
     <>
-      <Header />
+      {/* showHeader가 true일 때만 헤더 표시 */}
+      {showHeader && <Header />}
+      
+      {/* showPageLocation이 true일 때만 위치 표시 */}
       {showPageLocation && <PageLocation />}
+      
       <div>
         <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/globalsearch" element={<GlobalSearch />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/library" element={<MyLibrary />} />
-          <Route path="/my-bookDetail" element={<MyBookDetail />} />
-          <Route path="/bookinfo" element={<BookInfo />} />
-          <Route path="/info-bookDetail" element={<InfoBookDetail />} />
-          <Route path="/challengeRanking" element={<ChallengeRanking />} />
-          <Route path="/community" element={<CommunityList />} />
-          <Route path="/add-post" element={<AddPost />} />
-          <Route path="/detail-post/:postId/:postType" element={<DetailPost />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/friend/:id" element={<FriendLibrary />} />
-          <Route path="/notification" element={<Notification />} />
+          <Route path="/" element={<Intro />} />
+
+          {/* 로그인 필요 페이지 */}
+          <Route 
+            path="/home" 
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/library" 
+            element={
+              <ProtectedRoute>
+                <MyLibrary />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/my-bookDetail" 
+            element={
+              <ProtectedRoute>
+                <MyBookDetail />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/bookinfo" 
+            element={
+              <ProtectedRoute>
+                <BookInfo />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/info-bookDetail" 
+            element={
+              <ProtectedRoute>
+                <InfoBookDetail />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/challengeRanking" 
+            element={
+              <ProtectedRoute>
+                <ChallengeRanking />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/community" 
+            element={
+              <ProtectedRoute>
+                <CommunityList />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/add-post" 
+            element={
+              <ProtectedRoute>
+                <AddPost />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/detail-post/:postId/:postType" 
+            element={
+              <ProtectedRoute>
+                <DetailPost />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/mypage" 
+            element={
+              <ProtectedRoute>
+                <MyPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/friend/:id" 
+            element={
+              <ProtectedRoute>
+                <FriendLibrary />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/notification" 
+            element={
+              <ProtectedRoute>
+                <Notification />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/globalsearch" 
+            element={
+              <ProtectedRoute>
+                <GlobalSearch />
+              </ProtectedRoute>
+            } 
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
       <TopButton />
