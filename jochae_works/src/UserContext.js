1c4dde8e64b36abcca9a3cefc410a6eb4d4319bd
jochae <163301367+jochae@users.noreply.github.com>
2025-12-04 23:43:00 +0900

    token 포함으로 변경

--- jochae_works/src/UserContext.js
+++ jochae_works/src/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
@@ -7,9 +7,6 @@ export const UserProvider = ({ children }) => {
     const storedUser = localStorage.getItem("user");
     return storedUser ? JSON.parse(storedUser) : null;
   });
-
-
-  const [loading, setLoading] = useState(false);
 
   // 로그인 시 user 정보를 저장하고 localStorage에도 저장
   const login = (userData, token) => {
@@ -26,7 +23,7 @@ export const UserProvider = ({ children }) => {
   };
 
   return (
-    <UserContext.Provider value={{ user, setUser, login, logout, loading }}>
+    <UserContext.Provider value={{ user, setUser, login, logout }}>
       {children}
     </UserContext.Provider>
   );
