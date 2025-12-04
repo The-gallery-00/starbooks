b54b47052a07f1b6b9c1d8da6772118a91c0caf5
jochae <163301367+jochae@users.noreply.github.com>
2025-12-04 23:04:38 +0900

    token 같이 받아오도록 변경

--- jochae_works/src/UserContext.js
+++ jochae_works/src/UserContext.js
@@ -4,18 +4,19 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(() => {
-    // 새로고침 시 localStorage에서 user 정보 가져오기
     const storedUser = localStorage.getItem("user");
     return storedUser ? JSON.parse(storedUser) : null;
   });
+
 
   const [loading, setLoading] = useState(false);
 
   // 로그인 시 user 정보를 저장하고 localStorage에도 저장
   const login = (userData, token) => {
+    const newUser = { ...userData, token };
     localStorage.setItem("token", token);
-    localStorage.setItem("user", JSON.stringify(userData));
-    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(newUser));
+    setUser(newUser);
   };
 
   const logout = () => {
