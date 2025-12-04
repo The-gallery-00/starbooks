84fcfc3d4425537e23722fa51f9443a6ef22da29
Hyrax01 <piora0702@gmail.com>
2025-12-05 00:05:22 +0900

    recordId로 변경

--- starbooks/src/main/java/com/starbooks/service/reading/ReadingRecordService.java
+++ starbooks/src/main/java/com/starbooks/service/reading/ReadingRecordService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public interface ReadingRecordService {
     ReadingRecord save(ReadingRecord r);
-    ReadingRecord find(Long id);
-    void delete(Long id);
+    ReadingRecord find(Long recordId);
+    void delete(Long recordId);
     List<ReadingRecord> findByUserId(Long userId);
 
 }
