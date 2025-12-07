377c45591564eb563e59c0b0e70616d70da18106
SeEun <oseeun60@gmail.com>
2025-12-07 18:29:46 +0900

    comment 시간 수정

--- starbooks/src/main/java/com/starbooks/domain/community/Comment.java
+++ starbooks/src/main/java/com/starbooks/domain/community/Comment.java
@@ -5,6 +5,7 @@ import com.starbooks.domain.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -39,6 +40,7 @@ public class Comment {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
