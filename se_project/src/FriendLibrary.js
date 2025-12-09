55323b1ca779d2b869bf61434b762374d480e246
jochae <163301367+jochae@users.noreply.github.com>
2025-12-09 23:02:00 +0900

    FriendLibrary 테스트

--- se_project/src/FriendLibrary.js
+++ se_project/src/FriendLibrary.js
@@ -1,5 +1,5 @@
 // FriendLibrary.js
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import FriendProfile from "./FriendProfile";
 import FriendBookList from "./FriendBookList";
@@ -9,28 +9,35 @@ import "./FriendLibrary.css";
 export default function FriendLibrary() {
   const { id } = useParams();
 
-  // 실제 서비스에서는 id로 API 호출해서 친구 데이터 받아와야함
-  // 지금은 임시 더미 데이터 형태
   const friendData = {
     profile: {
-      nickname: "친구닉네임",
-      bio: "안녕하세요! 책 읽기 좋아합니다.",
+      nickname: "책책책을읽읍시다",
+      bio: "안녕하세요~",
       favoriteAuthors: ["헤르만 헤세"],
-      preferredGenres: ["소설", "철학"],
+      favoriteGenres: ["소설", "철학"],
     },
     books: {
-      reading: [],
+      reading: [
+        { id: 1, title: "데미안", author: "헤르만 헤세", progress: "50%", cover: "https://img1.daumcdn.net/thumb/R1280x0.fwebp/?fname=http://t1.daumcdn.net/brunch/service/user/7H8p/image/MsFtcopMcsD1h86nbkxGzcjv0mo.JPG" },],
       finished: [],
       wishlist: []
     },
-    records: []
+    records: [
+      { id: 1, bookTitle: "데미안", rating: 5, summary: "주인공의 성장 이야기", date: "2025/11/05" },
+      { id: 2, bookTitle: "죄와 벌", rating: 4, summary: "인간 심리와 죄의식", date: "2025/11/07" },
+      { id: 3, bookTitle: "노인과 바다", rating: 5, summary: "인간과 자연의 투쟁", date: "2025/11/10" },
+      { id: 4, bookTitle: "소설가 구보씨의 일일", rating: 3, summary: "소설가의 하루 이야기", date: "2025/11/12" }
+    ]
   };
 
   const handleFriendRequest = (user) => {
-    // 여기에 API 호출 가능
     console.log(`친구 신청: ${user.nickname}`);
     alert(`${user.nickname}님에게 친구 신청이 완료되었습니다.`);
   };
+
+  useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
 
   return (
     <div className="friend-library-container">
