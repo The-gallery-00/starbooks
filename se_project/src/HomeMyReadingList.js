795c74c9444dc5fbdbf80acb88462f8565fdf175
jochae <163301367+jochae@users.noreply.github.com>
2025-12-09 22:22:26 +0900

    테스트용

--- se_project/src/HomeMyReadingList.js
+++ se_project/src/HomeMyReadingList.js
@@ -6,9 +6,15 @@ export function HomeMyReadingList() {
   const navigate = useNavigate();
 
   const readingBooks = [
-    { id: 1, title: '데미안', author: '헤르만 헤세', progress: 65, cover: dummy },
-    { id: 2, title: '1984', author: '조지 오웰', progress: 42, cover: dummy },
-    { id: 3, title: '어린왕자', author: '생텍쥐페리', progress: 88, cover: dummy }
+    { 
+      id: 1, title: '소년이 온다', author: '한강', progress: '75', cover: "https://image.aladin.co.kr/product/4086/97/cover/8936434128_2.jpg", link: '#',
+      allPages: 216, 
+      description: '섬세한 감수성과 치밀한 문장으로 인간 존재의 본질을 탐구해온 작가 한강의 여섯번째 장편소설. 상처의 구조에 대한 투시와 천착의 서사를 통해 한강만이 풀어낼 수 있는 방식으로 1980년 5월을 새롭게 조명한다.'
+    },
+    { id: 2, title: '데미안', author: '헤르만 헤세', progress: '25', cover: "https://img1.daumcdn.net/thumb/R1280x0.fwebp/?fname=http://t1.daumcdn.net/brunch/service/user/7H8p/image/MsFtcopMcsD1h86nbkxGzcjv0mo.JPG", link: '#',
+        allPages: 200,
+        description: '2차 세계대전 중 많은 독일 젊은이들이 전장에 나가면서 군복 주머니 속에 품고 갔던 책. 어른이 되기 위해 보이지 않는 껍질을 깨고 고통스런 현실의 세계로 나서는 젊은이들을 그렸다. 지금까지도 젊은이들에게 통과의례처럼 읽히고 있는 명작을 새로 옮겼다.'
+       },
   ];
 
   return (
